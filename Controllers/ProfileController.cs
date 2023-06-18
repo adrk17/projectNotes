e095dc59e77d435912ac5cbb6f7a9ddb95c9bdae
Adrian <adkrupa@student.agh.edu.pl>
2023-06-18 23:40:39 +0200

    Change Password functionality

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -8,6 +8,7 @@ namespace projectNotes.Controllers
     public class ProfileController : Controller
     {
         private readonly ApplicationDatabaseContext _dbContext;
+        private readonly PasswordHash _passwordHash = new PasswordHash("sha256");
 
         public ProfileController(ApplicationDatabaseContext dbcontext)
         {
@@ -19,6 +20,47 @@ namespace projectNotes.Controllers
             return View("Index", new ProfileModel() {Username = HttpContext.Session.GetString("username")});
         }
 
-        
+        public IActionResult ChangePassword(ProfileModel model)
+        {
+            model.Username = HttpContext.Session.GetString("username");
+            if (ModelState.IsValid)
+            {
+                // did we find the user in the database?
+                var userInDB = _dbContext.Users.Where(u => u.Username == model.Username).FirstOrDefault();
+
+                bool passwordMatch = _passwordHash.ComparePasswords(model.Password, userInDB.Password);
+                Console.WriteLine(passwordMatch);
+                if (passwordMatch)
+                {
+                    if(model.NewPassword == model.NewPasswordRe)
+                    {
+                        
+                        userInDB.Password = _passwordHash.HashPassword(model.NewPassword);
+                        if(!_passwordHash.ComparePasswords(model.Password, userInDB.Password))
+                        {
+                            _dbContext.SaveChanges();
+                            return View("Index", new ProfileModel() { Username = HttpContext.Session.GetString("username"), SuccessMsg = "Password changed successfully!" });
+                        }
+                        else
+                        {
+                            
+                            ModelState.AddModelError("Password", "Old and new passwords are the same!");
+                            return View("Index", model);
+                        }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("NewPassword", "New passwords do not match");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("Password", "Invalid password");
+                }
+                return View("Index", model);
+            }
+            Console.WriteLine("not valid");
+            return View("Index", model);
+        }
     }
 }
--- Models/ProfileModel.cs
+++ Models/ProfileModel.cs
@@ -1,6 +1,19 @@
-﻿namespace projectNotes.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace projectNotes.Models;
 
 public class ProfileModel
 {
-    public string Username { get; set; }
+    public string? Username { get; set; }
+    [Required]
+    [MinLength(5)]
+    public string Password { get; set; }
+    [Required]
+    [MinLength(5)]
+    public string NewPassword { get; set; }
+    [Required]
+    [MinLength(5)]
+    public string NewPasswordRe { get; set; }
+
+    public string? SuccessMsg { get; set;}
 }
