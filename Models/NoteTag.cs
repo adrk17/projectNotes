8c911cdc19b2d7f407b12743f40a9b62ed07e0fd
Adrian <adkrupa@student.agh.edu.pl>
2023-06-18 16:11:40 +0200

    idk

--- Models/NoteTag.cs
+++ Models/NoteTag.cs
@@ -1,11 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace projectNotes.Models
 {
     public class NoteTag
     {
         [Key]
+        [Column(Order = 1)]]
         public int NoteID { get; set; }
+        [Key]
+        [Column(Order = 2)]
         public int TagID { get; set; }
     }
 }
